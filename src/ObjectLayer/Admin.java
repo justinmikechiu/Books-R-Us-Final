090d34ae2307328fd44f340770852c514e48cff7
Justin Chiu <jmc85873@uga.edu>
2017-07-06 15:38:22 -0400

    Create Admin.java

---
+++ src/ObjectLayer/Admin.java
@@ -0,0 +1,16 @@
+package ObjectLayer;
+
+public class Admin {
+	private User userinfo;// Admin just user with special methods and functions 
+
+	public User getUserinfo() {
+		return userinfo;
+	}
+
+	public void setUserinfo(User userinfo) {
+		this.userinfo = userinfo;
+	}
+	
+	
+	
+}
